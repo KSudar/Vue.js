b71df751de4907e38710f04debd0a03c2aa59cb3
Kresimir <kresimir@codeasy.com>
2019-03-25 12:42:46 +0100

    kemijam assignment 7

---
+++ Section 8 Code (Start)/Assignment 7 - component communication/src/components/Server/ServerDetails.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <div v-if="!server"><p>Server Details are currently not updated</p></div>
+        <div v-else><p>Server Details</p>
+        <p>Server #{{ server.id }} status is {{server.status}}</p></div>
+    </div>
+
+</template>
+
+<script>
+export default {
+   props: {
+      server: {
+         type: Object
+      }
+   },
+   methods: {
+      switchName() {
+         return this.myName
+            .split("")
+            .reverse()
+            .join("");
+      }
+   },
+   created() {
+      eventBus.$on('ageWasEdited', (age) => {
+         this.userAge = age;
+      });
+   }
+};
+
+</script>
+
+<style>
+
+</style>
---
+++ Section 8 Code (Start)/Assignment 7 - component communication/src/components/Server/Servers.vue
@@ -0,0 +1,26 @@
+<template>
+   <div class="col-xs-12 col-sm-6">
+      <ul class="list-group">
+         <li class="list-group-item" v-for="(server, index) in servers" :key="index" :server = "servers[index]">Server #{{ server.id }}</li>
+      </ul>
+   </div>
+</template>
+
+<script>
+export default {
+   data: function() {
+      return {
+         servers: [
+            { id: 1, status: "Normal" },
+            { id: 2, status: "Critical" },
+            { id: 3, status: "Unknown" },
+            { id: 4, status: "Normal" },
+            { id: 5, status: "Unknown" }
+         ]
+      };
+   }
+};
+</script>
+
+<style>
+</style>
