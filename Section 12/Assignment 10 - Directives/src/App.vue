4c3f36399c6d558dc82e697cab066b5dd2566262
Kresimir <kresimir@codeasy.com>
2019-04-03 17:08:24 +0200

    Section 12 Finished really

---
+++ Section 12/Assignment 10 - Directives/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+   <div class="container">
+      <div class="row">
+         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+            <h1>Directives Exercise</h1>
+            <button class="btn:btn-primary" v-myOn:click="clicked">Click me</button>
+            <hr>
+            <div style="width:100px; height: 100px; background-color: lightgreen;" v-myOn:mouseenter="mouseEnter" v-myOn:mouseleave="mouseLeave"></div>
+         </div>
+      </div>
+   </div>
+</template>
+
+<script>
+   export default {
+      directives: {
+         'myOn': {
+            bind(el, binding) {
+               const type = binding.arg;
+               const fn = binding.value;
+               el.addEventListener(type, fn);
+            }
+         }
+      },
+      methods: {
+         clicked() {
+            alert('works')
+         },
+         mouseEnter(){
+             console.log('Mouse entered!');
+             
+         },
+         mouseLeave(){
+             console.log('Mouse left!');
+             
+         }
+      }
+   }
+</script>
+
+<style>
+</style>
