48daeb621b6b7e0a774245011c9293925db3c491
Kresimir <kresimir@codeasy.com>
2020-03-11 12:27:52 +0100

    L16S255

---
+++ Section 16/Section Code (Start)/src/components/404.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<h1>Error 404: Page not found</h1>
+		<hr />
+		<button class="btn btn-primary" @click="navigateToHome">Go to Home</button>
+	</div>
+</template>
+<script>
+export default {
+	methods: {
+		navigateToHome() {
+			this.$router.push({ name: 'home'});
+		},
+	},
+};
+</script>
--- Section 16/Section Code (Start)/src/components/user/UserDetail.vue
+++ Section 16/Section Code (Start)/src/components/user/UserDetail.vue
@@ -2,11 +2,23 @@
 	<div>
 		<h3>Some User Details</h3>
 		<p>User loaded has ID: {{ $route.params.id }}</p>
-		<router-link
-			tag="button"
-			class="btn btn-primary"
-			:to="{ name: 'userEdit', params: { id: $route.params.id} }"
+		<br />
+		<router-link tag="button" class="btn btn-primary" :to="link"
 			>Edit user</router-link
 		>
 	</div>
 </template>
+<script>
+export default {
+	data() {
+		return {
+			link: {
+				name: 'userEdit',
+				params: { id: this.$route.params.id },
+				query: { q: 100, locale: 'en' },
+				hash: '#data',
+			},
+		};
+	},
+};
+</script>
