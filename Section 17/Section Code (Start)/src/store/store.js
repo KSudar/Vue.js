238c455dcbe766be7410ae3a66909fdb5e95c3fd
Kresimir <kresimir@codeasy.com>
2020-03-12 17:06:48 +0100

    Section pred kraj nastavit će samo

--- Section 17/Section Code (Start)/src/store/store.js
+++ Section 17/Section Code (Start)/src/store/store.js
@@ -1,10 +1,28 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-
+import counter from './modules/counter'
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
 	state: {
-		counter: 0,
+		value: 0,
+	},
+	getters: {
+		valueGet: state => {
+			return state.value;
+		},
+	},
+	mutations: {
+		updateValue: (state, payload) => {
+			state.value = payload;
+		},
+	},
+	actions: {
+		updateValue({ commit }, payload) {
+			commit('updateValue', payload);
+		},
+	},
+	modules: {
+		counter
 	}
 });
